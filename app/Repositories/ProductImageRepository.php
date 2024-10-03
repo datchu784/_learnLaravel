cc518d8b48aae3504d9e46f224ff9c8926869192
dacthu784 <125370292+dacthu784@users.noreply.github.com>
2024-10-03 14:22:17 +0700

    chinh anh

--- app/Repositories/ProductImageRepository.php
+++ app/Repositories/ProductImageRepository.php
@@ -22,12 +22,21 @@ class ProductImageRepository extends BaseRepository implements IProductImageRepo
             $product_image = $this->getById($id);
             $original_main = $this->getAll()->where('product_id', $product_image->product_id)
             ->where('main', 1)->first();
+            if($original_main !=null)
+            {
+                $original_main->main = 0;
+                $product_image->main = 1;
 
-            $original_main->main = 0;
-            $product_image->main = 1;
+                $product_image->save();
+                $original_main->save();
 
-            $product_image->save();
-            $original_main->save();
+            }
+            else{
+                $product_image->main = 1;
+                $product_image->save();
+            }
+
+
 
             return $product_image;
             DB::commit();

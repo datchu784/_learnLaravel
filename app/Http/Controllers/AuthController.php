980870a2d9c94a5d419410e2f43407193504e19d
dacthu784 <125370292+dacthu784@users.noreply.github.com>
2024-10-09 08:51:08 +0700

    fix xíu

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -55,7 +55,7 @@ class AuthController extends Controller
     {
 
         return response()->json([
-            'access_token' => $token,
+            'token' => $token,
             'token_type' => 'bearer',
 
         ]);

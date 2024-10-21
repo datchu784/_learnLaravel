c15bb0b997ab1648d0321e9398d1bc46f9cd3379
dacthu784 <125370292+dacthu784@users.noreply.github.com>
2024-10-21 13:55:00 +0700

    fix vui vui

--- app/Repositories/ProductAttributeRepository.php
+++ app/Repositories/ProductAttributeRepository.php
@@ -63,7 +63,7 @@ class ProductAttributeRepository extends BaseRepository implements IProductAttri
             ->groupBy('combination_id')
             ->map(function ($group) {
                 return [
-                    'producombination_id' => $group->first()->combination_id,
+                    'product_combination_id' => $group->first()->combination_id,
                     'product_name' => $group->first()->product_name,
                     'attributes' => $group->pluck('attribute_value', 'attribute_name')->toArray(),
                     'product_price' => $group->first()->product_price,
